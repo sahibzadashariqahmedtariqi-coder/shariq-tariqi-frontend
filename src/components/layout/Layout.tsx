510d92ff4651d6f0df3f9c80529c8bd24ddc4656
sp23-bcs-118-Subhan-Ahmed <sp23-bcs-118@cuilahore.edu.pk>
2026-02-23 12:32:34 +0500

    SEO Fix: Dynamic canonical URLs per page - fixes Google duplicate content issue

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,25 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { Helmet } from 'react-helmet-async'
 import Header from './Header'
 import Footer from './Footer'
 import WhatsAppButton from '../ui/WhatsAppButton'
 
+function DynamicCanonical() {
+  const { pathname } = useLocation()
+  const baseUrl = 'https://www.sahibzadashariqahmedtariqi.com'
+  const canonicalUrl = `${baseUrl}${pathname === '/' ? '' : pathname}`
+
+  return (
+    <Helmet>
+      <link rel="canonical" href={canonicalUrl} />
+    </Helmet>
+  )
+}
+
 export default function Layout() {
   return (
     <div className="flex min-h-screen flex-col">
+      <DynamicCanonical />
       <Header />
       <main className="flex-1">
         <Outlet />
